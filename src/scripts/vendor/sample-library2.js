0d8b81e8438d5a61ee0274de321e2573bb5fe245
Ani <anita.nenova8@gmail.com>
2021-03-30 18:05:54 +0300

    responsive styles for header and info section

--- src/scripts/vendor/sample-library2.js
+++ src/scripts/vendor/sample-library2.js
@@ -1,2 +1,14 @@
-const c=1;
-console.log(c);
+const mobileMenu = document.querySelector(".menu-mobile");
+const nav = document.querySelector(".header-navigation");
+
+mobileMenu.addEventListener("click", function (e) {
+  let current = e.target.classList;
+
+  if (current.contains("menu-open")) {
+    current.remove("menu-open");
+    nav.style.display = "none";
+  } else {
+    current.add("menu-open");
+    nav.style.display = "block";
+  }
+});
