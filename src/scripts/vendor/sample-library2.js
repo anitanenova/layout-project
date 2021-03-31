fac6dafcb098b6379df177abf6d121c5e79ee97c
Ani <anita.nenova8@gmail.com>
2021-03-31 11:44:28 +0300

    added animation to header

--- src/scripts/vendor/sample-library2.js
+++ src/scripts/vendor/sample-library2.js
@@ -6,9 +6,13 @@ mobileMenu.addEventListener("click", function (e) {
 
   if (current.contains("menu-open")) {
     current.remove("menu-open");
-    nav.style.display = "none";
+    // nav.style.display = "none";
+    // nav.style.transition = "0.5s";
+    nav.style.transform = "translateX(-100%)";
   } else {
     current.add("menu-open");
-    nav.style.display = "block";
+    // nav.style.display = "block";
+    //nav.style.transition = "0.5s";
+    nav.style.transform = "translateX(0)";
   }
 });
